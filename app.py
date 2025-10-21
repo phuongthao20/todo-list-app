59cfbe6f914ae58ddcbcf20764beba8f5c626b96
Phuong Thao <ngphgthao20@gmail.com>
2025-10-21 10:52:58 +0700

    feat: Add list-task function

--- app.py
+++ app.py
@@ -9,3 +9,13 @@ if __name__ == "__main__":
  print("Chào mừng đến với ứng dụng To-Do List!")
  add_task("Học bài Git và GitHub")
  add_task("Làm bài tập thực hành ở nhà")
+def list_tasks():
+ """ In ra tất cả các công việc trong danh sách tasks"""
+ if not tasks:
+  print("Danh sách công việc trống.")
+ else:
+  print("Danh sách công việc:")
+  for i,task in enumerate (tasks, start=1):
+   print(f"{i}. {task}")
+if __name__  == " __main__":
+ list_tasks()
